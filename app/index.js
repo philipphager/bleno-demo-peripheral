f09d60f83b2a70df29d172059efa5889d0531b35
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-11-03 10:33:05 +0100

    Bleno Hello World / Echo Tutorial

--- app/index.js
+++ app/index.js
@@ -1,1 +1,33 @@
 // app/index.js
+var bleno = require('bleno');
+
+var BlenoPrimaryService = bleno.PrimaryService;
+
+var HelloWorldCharacteristic = require('./characteristic');
+
+console.log('bleno - hello world service');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('hello world service', ['ec00']);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new HelloWorldCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
